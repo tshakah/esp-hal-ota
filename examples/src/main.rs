af462d90037b8c518b0fce273cfb3f463480d993
filipton <filipton12@gmail.com>
2024-11-20 14:10:41 +0100

    chore: update example to newest esp-hal

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -11,11 +11,13 @@ use esp_backtrace as _;
 use esp_hal::{prelude::*, timer::timg::TimerGroup};
 use esp_hal_ota::Ota;
 use esp_storage::FlashStorage;
-use esp_wifi::wifi::{
-    ClientConfiguration, Configuration, WifiController, WifiDevice, WifiEvent, WifiStaDevice,
-    WifiState,
+use esp_wifi::{
+    wifi::{
+        ClientConfiguration, Configuration, WifiController, WifiDevice, WifiEvent, WifiStaDevice,
+        WifiState,
+    },
+    EspWifiController,
 };
-use static_cell::make_static;
 
 const WIFI_SSID: &'static str = env!("SSID");
 const WIFI_PSK: &'static str = env!("PSK");
@@ -25,6 +27,15 @@ const RX_BUFFER_SIZE: usize = 16384;
 const TX_BUFFER_SIZE: usize = 16384;
 static mut TX_BUFF: [u8; TX_BUFFER_SIZE] = [0; TX_BUFFER_SIZE];
 static mut RX_BUFF: [u8; RX_BUFFER_SIZE] = [0; RX_BUFFER_SIZE];
+
+macro_rules! mk_static {
+    ($t:ty,$val:expr) => {{
+        static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
+        #[deny(unused_attributes)]
+        let x = STATIC_CELL.uninit().write(($val));
+        x
+    }};
+}
 
 #[main]
 async fn main(spawner: Spawner) {
@@ -38,13 +49,11 @@ async fn main(spawner: Spawner) {
 
     let rng = esp_hal::rng::Rng::new(peripherals.RNG);
     let timg1 = TimerGroup::new(peripherals.TIMG1);
-    let init = esp_wifi::init(
-        esp_wifi::EspWifiInitFor::Wifi,
-        timg1.timer0,
-        rng.clone(),
-        peripherals.RADIO_CLK,
-    )
-    .unwrap();
+
+    let init = &*mk_static!(
+        EspWifiController<'static>,
+        esp_wifi::init(timg1.timer0, rng.clone(), peripherals.RADIO_CLK).unwrap()
+    );
 
     let timg0 = TimerGroup::new(peripherals.TIMG0);
     esp_hal_embassy::init(timg0.timer0);
@@ -56,12 +65,15 @@ async fn main(spawner: Spawner) {
     let config = Config::dhcpv4(Default::default());
     let seed = 69420;
 
-    let stack = &*make_static!(Stack::new(
-        wifi_interface,
-        config,
-        make_static!(StackResources::<3>::new()),
-        seed,
-    ));
+    let stack = &*mk_static!(
+        Stack<WifiDevice<'_, WifiStaDevice>>,
+        Stack::new(
+            wifi_interface,
+            config,
+            mk_static!(StackResources<3>, StackResources::<3>::new()),
+            seed,
+        )
+    );
 
     spawner
         .spawn(connection(controller, stack))
@@ -158,9 +170,9 @@ async fn connection(
     stack: &'static Stack<WifiDevice<'static, WifiStaDevice>>,
 ) {
     log::info!("start connection task");
-    log::info!("Device capabilities: {:?}", controller.get_capabilities());
+    log::info!("Device capabilities: {:?}", controller.capabilities());
     loop {
-        if esp_wifi::wifi::get_wifi_state() == WifiState::StaConnected {
+        if esp_wifi::wifi::wifi_state() == WifiState::StaConnected {
             // wait until we're no longer connected
             controller.wait_for_event(WifiEvent::StaDisconnected).await;
             Timer::after(Duration::from_millis(5000)).await
@@ -173,12 +185,12 @@ async fn connection(
             });
             controller.set_configuration(&client_config).unwrap();
             log::info!("Starting wifi");
-            controller.start().await.unwrap();
+            controller.start_async().await.unwrap();
             log::info!("Wifi started!");
         }
         log::info!("About to connect...");
 
-        match controller.connect().await {
+        match controller.connect_async().await {
             Ok(_) => {
                 log::info!("Wifi connected!");
 
