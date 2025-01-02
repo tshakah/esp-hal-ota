8ec58c88013a81f8bfb5eaff1448a1185906c06e
filipton <filipton12@gmail.com>
2025-01-02 15:46:23 +0100

    wip: port esp32c2

--- src/mmu_ll/mod.rs
+++ src/mmu_ll/mod.rs
@@ -1,4 +1,29 @@
+#[cfg(feature = "esp32s2")]
+mod esp32s2;
+#[cfg(feature = "esp32s2")]
+pub use esp32s2::*;
+
 #[cfg(feature = "esp32s3")]
 mod esp32s3;
 #[cfg(feature = "esp32s3")]
 pub use esp32s3::*;
+
+#[cfg(feature = "esp32c2")]
+mod esp32c2;
+#[cfg(feature = "esp32c2")]
+pub use esp32c2::*;
+
+#[cfg(feature = "esp32c3")]
+mod esp32c3;
+#[cfg(feature = "esp32c3")]
+pub use esp32c3::*;
+
+#[cfg(feature = "esp32c6")]
+mod esp32c6;
+#[cfg(feature = "esp32c6")]
+pub use esp32c6::*;
+
+#[cfg(feature = "esp32h2")]
+mod esp32h2;
+#[cfg(feature = "esp32h2")]
+pub use esp32h2::*;
